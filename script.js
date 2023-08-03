3321c76d9cca2d45faf49e5e2fac12b3e5c88dc8
Fan Yang <yangfanhewangzheng@gmail.com>
2023-08-02 23:52:46 -0400

    part of hw git push -u origin git push origin master cd..

--- script.js
+++ script.js
@@ -17,22 +17,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+document.querySelector('#main-title').textContent = "Hi! I am DOM Toretto. Welcome!"
 
   // Part 2
+document.querySelector('body').style.backgroundColor = "yellow"
+  // Part 3 cannot work
+const favoriteThings = document.querySelector("#favorite-things")
+favoriteThings.classList.remove("Staring-pepole-down")
 
-
-  // Part 3
-
-
+ 
   // Part 4
-
-
-  // Part 5
-
-
-  // Part 6
-
+const specialTitleArr = document.querySelectorAll(".special-title");
+for(i = 0; i < specialTitleArr.length; i++ ){
+  specialTitleArr[i].style.fontSize = "2rem"
+}
+  // Part 5 can not work as well
+const pastRaces = document.querySelector("#past-races")
+pastRaces.classList.remove("Chicago")
+  // Part 6 not work, seems like didn't select the correct item
+ const addli = pastRaces.createElement("li")
+ addli.textContent = "Boston"
+ pastRaces.appendChild(addli)
 
   // Part 7
 
